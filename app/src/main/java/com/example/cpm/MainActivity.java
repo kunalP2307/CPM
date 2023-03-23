0acc2695e6559fc0ebfc87e79e15041a27921b03
kunalP2307 <kunalpatil2307@gmail.com>
2023-03-23 23:36:32 +0530

    Connected to firebase

--- app/src/main/java/com/example/cpm/MainActivity.java
+++ app/src/main/java/com/example/cpm/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.cpm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
+        firebaseAuth.createUserWithEmailAndPassword("kunalpatil2307@gmail.com", "Acc0unt$")
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(MainActivity.this, "Complete", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
     }
 }
